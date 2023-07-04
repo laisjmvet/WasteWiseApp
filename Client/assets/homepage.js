c3f40f8431e65e165b2c04dc5e7ecfb11f952c19
Gilheim <Guy.vernon@mac.com>
2023-07-04 15:31:08 +0100

    started creating recycling popup

--- Client/assets/homepage.js
+++ Client/assets/homepage.js
@@ -45,8 +45,8 @@ async function setUpPage(userData) {
         adminButton.name = "admin_button"
         adminButton.textContent= "admin"
         
-        const body = document.querySelector('body')
-        body.appendChild(adminButton)   
+        const buttonsDiv = document.getElementsByName("main_buttons")[0]
+        buttonsDiv.appendChild(adminButton)   
     }
 }
 
@@ -69,7 +69,7 @@ async function displayBins (data) {
 
     const binInfo = document.createElement("p")
     binInfo.id= "bin_info"
-    binInfo.textContent = `Your bins will be collected on ${weekday}`
+    binInfo.textContent = `Your bins will be collected on ${weekday}.`
     binCollectionBox.appendChild(binInfo)
 
     const binImg = document.createElement("img")
@@ -78,14 +78,55 @@ async function displayBins (data) {
     binImg.id = "bin_img"
     binCollectionBox.appendChild(binImg)
 
-    const body = document.querySelector('body')
-    body.appendChild(binCollectionBox)
+    const binFlorin = document.getElementById("bin_and_user")
+    binFlorin.appendChild(binCollectionBox)
 }
 
 const recycleButton = document.getElementsByName("RecyclingButton")[0]
 recycleButton.addEventListener("click", openRecyclingMenu)
 
 async function openRecyclingMenu() {
-    
+    const recyclingMenuContainer = document.createElement("div")
+    recyclingMenuContainer.id = "recycling_menu_container"
+
+    const recyclingMenu = document.createElement("main")
+    recyclingMenu.id = "recycling_menu"
+    recyclingMenuContainer.appendChild(recyclingMenu)
+
+    const backButton = document.createElement("button")
+    backButton.textContent = "Back"
+    backButton.name = "back_button"
+    recyclingMenu.appendChild(backButton)
+
+    const recyclingForm = document.createElement("form")
+    recyclingForm.name = "recycling_form"
+
+    const searchBar = document.createElement("input")
+    searchBar.type = "text"
+    searchBar.placeholder = "Search..."
+    searchBar.name = "search_bar"
+    searchBar.addEventListener('keydown', obtainRecycleItems)
+
+    recyclingForm.appendChild(searchBar)
+    recyclingMenu.appendChild(recyclingForm)
+
+    const body = document.querySelector('body')
+    body.appendChild(recyclingMenuContainer)
+}
+
+
+async function obtainRecycleItems() {
+    const searchContents = document.getElementsByName("search_bar")[0].value
+    if(searchContents.length>=3){
+        try {
+            const data = await fetch(`http://localhost:3000/object/input/${searchContents}`)
+            if(data.ok) {
+                const searchItems = await data.json()
+                console.log(searchItems)
+            }
+        } catch(e){
+            console.log(e)
+        }
+    }
 }
 

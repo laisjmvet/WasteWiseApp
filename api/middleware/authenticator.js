e25bc6987b847afbb4390ae9ef942eb621e3441b
laisjmvet <laisjmvet@gmail.com>
2023-07-05 13:15:05 +0100

    working on the token

--- api/middleware/authenticator.js
+++ api/middleware/authenticator.js
@@ -1,25 +1,20 @@
 const Token = require("../models/Token");
-const User = require("../controllers/user.js");
 
 async function authenticator(req, res, next) {
-  try {
-    const userToken = req.headers.authorization;
+    try {
+        const userToken = req.headers["authorization"];
+        console.log(req.body, "<<<<<<<<<<<<<<")
+        if (userToken == "null") {
+            throw new Error("User not authenticated.");
+        } else {
+            const validToken = await Token.getOneByToken(userToken);
 
-    if (!userToken) {
-      throw new Error("User not authenticated.");
-    } else {
-      const validToken = await Token.getOneByToken(userToken);
+            next();
+        }
 
-      if (!validToken) {
-        throw new Error("Invalid token.");
-      } else {
-        req.user_id = validToken.user_id;
-        next();
-      }
+    } catch (err) {
+        res.status(403).json({ error: err.message });
     }
-  } catch (err) {
-    res.status(403).json({ error: err.message });
-  }
 }
 
 module.exports = authenticator;

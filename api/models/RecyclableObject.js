f4bdea7a9ddd27db73f3fa938a1053472ad236d8
laisjmvet <laisjmvet@gmail.com>
2023-07-06 14:42:32 +0100

    trycatch materialstype

--- api/models/RecyclableObject.js
+++ api/models/RecyclableObject.js
@@ -9,22 +9,31 @@ class RecyclableObject {
     }
 
     static async getAll() {
-        const response = await db.query("SELECT * FROM recycling_object");
-        return response.rows.map(o => new RecyclableObject(o));
+        try {
+            const response = await db.query("SELECT * FROM recycling_object");
+            return response.rows.map(o => new RecyclableObject(o));
+        } catch (error) {
+            console.log(error);
+        }        
     }
 
     static async getOneById(id) {
-        const response = await db.query("SELECT * FROM recycling_object WHERE object_id = $1", [id]);
-        if (response.rows.length != 1) {
-            throw new Error("Unable to locate item.");
+        try {
+            const response = await db.query("SELECT * FROM recycling_object WHERE object_id = $1", [id]);
+            return new RecyclableObject(response.rows[0]);
+        } catch (error) {
+            console.log(error);
         }
-        return new RecyclableObject(response.rows[0]);
     }
 
     static async create(data) {
-        const { name, material_type_id, bin_type_id } = data;
-        let response = await db.query("INSERT INTO recycling_object (name, material_type_id, bin_type_id) VALUES ($1, $2, $3) RETURNING *;", [name, material_type_id, bin_type_id]);
-        return new RecyclableObject(response.rows[0]);
+        try {
+            const { name, material_type_id, bin_type_id } = data;
+            const response = await db.query("INSERT INTO recycling_object (name, material_type_id, bin_type_id) VALUES ($1, $2, $3) RETURNING *;", [name, material_type_id, bin_type_id]);
+            return new RecyclableObject(response.rows[0]);
+        } catch (error) {
+            console.log(error);
+        }        
     }
 
     static async searchInput(input) {        
@@ -37,14 +46,22 @@ class RecyclableObject {
     }
 
     async destroy() {
-        let response = await db.query("DELETE FROM recycling_object WHERE object_id = $1 RETURNING *;", [this.id]);
-        return new RecyclableObject(response.rows[0]);
+        try {
+            let response = await db.query("DELETE FROM recycling_object WHERE object_id = $1 RETURNING *;", [this.id]);
+            return new RecyclableObject(response.rows[0]);            
+        } catch (error) {
+            console.log(error);
+        }
     }
 
-    async update(data) {        
-        const {name, material_type_id, bin_type_id} = data;        
-        const response = await db.query("UPDATE recycling_object SET name = $1, material_type_id = $2, bin_type_id =$3 WHERE object_id = $4 RETURNING *;", [name, material_type_id, bin_type_id, this.id]);
-        return new RecyclableObject(response.rows[0]);
+    async update(data) {      
+        try {
+            const {name, material_type_id, bin_type_id} = data;        
+            const response = await db.query("UPDATE recycling_object SET name = $1, material_type_id = $2, bin_type_id =$3 WHERE object_id = $4 RETURNING *;", [name, material_type_id, bin_type_id, this.id]);
+            return new RecyclableObject(response.rows[0]);            
+        } catch (error) {
+            console.log(error);
+        }  
     }
 }
 module.exports = RecyclableObject;

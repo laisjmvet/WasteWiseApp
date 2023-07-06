3c3fa41c06d8aa72d55a00eb058d59fb88bd9670
laisjmvet <laisjmvet@gmail.com>
2023-07-06 15:01:50 +0100

    trycatch bin

--- api/models/Bin.js
+++ api/models/Bin.js
@@ -7,43 +7,61 @@ class Bin {
     }
 
     static async getAll() {
-        const response = await db.query("SELECT * FROM bin_types");
-        return response.rows.map(a => new Bin(a));
+        try {
+            const response = await db.query("SELECT * FROM bin_types");
+            return response.rows.map(a => new Bin(a));
+        } catch (error) {
+            console.log(error);
+        }        
     }
 
     static async getOneByName(name) {
-        const response = await db.query("SELECT * FROM bin_types WHERE bin_type_name ILIKE '%' || $1 || '%'", [name]);
-        if (response.rows.length != 1) {
-            throw new Error("Unable to locate bin.");
+        try {
+            const response = await db.query("SELECT * FROM bin_types WHERE bin_type_name ILIKE '%' || $1 || '%'", [name]);
+            return response.rows;
+        } catch (error) {
+            
         }
-        return response.rows;
     }
 
     static async getOneById(id) {
-        const response = await db.query("SELECT * FROM bin_types WHERE bin_type_id = $1", [id]);
-        if (response.rows.length != 1) {
-            throw new Error("Unable to locate bin.");
+        try {
+           const response = await db.query("SELECT * FROM bin_types WHERE bin_type_id = $1", [id]);
+           return new Bin(response.rows[0]);
+        } catch (error) {
+            console.log(error);
         }
-        return new Bin(response.rows[0]);
     }
 
     static async create(data) {
-        const {bin_type_name } = data;
-        let response = await db.query("INSERT INTO bin_types (bin_type_name) VALUES ($1) RETURNING *;", [bin_type_name]);
-        return new Bin(response.rows[0]);
+        try {
+            const {bin_type_name } = data;
+            const response = await db.query("INSERT INTO bin_types (bin_type_name) VALUES ($1) RETURNING *;", [bin_type_name]);
+            return new Bin(response.rows[0]);
+        } catch (error) {
+            console.log(error);
+        }        
     }
 
     async destroy() {
-        await db.query("DELETE FROM collect_days WHERE bin_type_id = $1 RETURNING *;", [this.bin_type_id]);
-        await db.query("UPDATE recycling_object SET bin_type_id = $1 RETURNING *;", [8]);
-        const response = await db.query("DELETE FROM bin_types WHERE bin_type_id = $1 RETURNING *;", [this.bin_type_id]);
-        return new Bin(response.rows[0]);
+        try {
+            await db.query("DELETE FROM collect_days WHERE bin_type_id = $1 RETURNING *;", [this.bin_type_id]);
+            await db.query("UPDATE recycling_object SET bin_type_id = $1 RETURNING *;", [8]);
+            const response = await db.query("DELETE FROM bin_types WHERE bin_type_id = $1 RETURNING *;", [this.bin_type_id]);
+            return new Bin(response.rows[0]);
+        } catch (error) {
+            console.log(error);
+        }        
     }
 
     async update(data) {        
-        const {bin_type_name} = data;        
-        const response = await db.query("UPDATE bin_types SET bin_type_name = $1 WHERE bin_type_id = $2 RETURNING *;", [bin_type_name, this.bin_type_id]);
-        return new Bin(response.rows[0]);
+        try {
+            const {bin_type_name} = data;        
+            const response = await db.query("UPDATE bin_types SET bin_type_name = $1 WHERE bin_type_id = $2 RETURNING *;", [bin_type_name, this.bin_type_id]);
+            return new Bin(response.rows[0]);
+        } catch (error) {
+            console.log(error);
+        }        
     }
 }
 module.exports = Bin;

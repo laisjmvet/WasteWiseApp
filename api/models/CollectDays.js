265bd7ec899464e60c4947738e3f0e2c44f73355
laisjmvet <laisjmvet@gmail.com>
2023-07-06 14:55:13 +0100

    trycatch materialstype

--- api/models/CollectDays.js
+++ api/models/CollectDays.js
@@ -9,41 +9,60 @@ class CollectDays {
     }
 
     static async getAll() {
-        const response = await db.query("SELECT * FROM collect_days");
-        return response.rows.map(c => new CollectDays(c));
+        try {
+            const response = await db.query("SELECT * FROM collect_days");
+            return response.rows.map(c => new CollectDays(c));
+        } catch (error) {
+            console.log(error);
+        }        
     }
 
     static async getOneById(id) {
-        const response = await db.query("SELECT * FROM collect_days WHERE collect_day_id = $1", [id]);
-        if (response.rows.length != 1) {
-            throw new Error("Unable to locate collection day.");
-        }
-        return new CollectDays(response.rows[0]);
+        try {
+            const response = await db.query("SELECT * FROM collect_days WHERE collect_day_id = $1", [id]);
+            return new CollectDays(response.rows[0]);
+        } catch (error) {
+            console.log(error);
+        }        
     }
 
     static async getByZoneId(zone_id) {
-        const response = await db.query("SELECT * FROM collect_days WHERE zone_id = $1", [zone_id]);
-        if (response.rows.length < 1) {
-            throw new Error("Unable to locate collect_day.");
+        try {
+            const response = await db.query("SELECT * FROM collect_days WHERE zone_id = $1", [zone_id]);
+            return response.rows;
+        } catch (error) {
+            console.log(error);
         }
-        return response.rows;
     }
 
     static async create(data) {
-        const { zone_id, bin_type_id, weekday_id } = data;
-        let response = await db.query("INSERT INTO collect_days (zone_id, bin_type_id, weekday_id) VALUES ($1, $2, $3) RETURNING *;", [zone_id, bin_type_id, weekday_id]);
-        return new CollectDays(response.rows[0]);
+        try {
+            const { zone_id, bin_type_id, weekday_id } = data;
+            const response = await db.query("INSERT INTO collect_days (zone_id, bin_type_id, weekday_id) VALUES ($1, $2, $3) RETURNING *;", [zone_id, bin_type_id, weekday_id]);
+            return new CollectDays(response.rows[0]);
+        } catch (error) {
+            console.log(error);
+        }        
     }
 
     async destroy() {
-        let response = await db.query("DELETE FROM collect_days WHERE collect_day_id = $1 RETURNING *;", [this.id]);
-        return new CollectDays(response.rows[0]);
+        try {
+            let response = await db.query("DELETE FROM collect_days WHERE collect_day_id = $1 RETURNING *;", [this.id]);
+            return new CollectDays(response.rows[0]);
+        } catch (error) {
+            console.log(error);
+        }        
     }
 
-    async update(data) {        
-        const {zone_id, bin_type_id, weekday_id} = data;        
-        const response = await db.query("UPDATE collect_days SET zone_id = $1, bin_type_id =$2, weekday_id = $3 WHERE collect_day_id = $4 RETURNING *;", [zone_id, bin_type_id, weekday_id, this.id]);
-        return new CollectDays(response.rows[0]);
+    async update(data) {  
+        try {
+            const {zone_id, bin_type_id, weekday_id} = data;        
+            const response = await db.query("UPDATE collect_days SET zone_id = $1, bin_type_id =$2, weekday_id = $3 WHERE collect_day_id = $4 RETURNING *;", [zone_id, bin_type_id, weekday_id, this.id]);
+            return new CollectDays(response.rows[0]);
+        } catch (error) {
+            console.log(error);            
+        }      
+        
     }
 }
 module.exports = CollectDays;

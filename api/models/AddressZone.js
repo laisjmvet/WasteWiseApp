b895a23296d1ff54d70e5382d57fad91675f0b62
laisjmvet <laisjmvet@gmail.com>
2023-07-06 15:08:50 +0100

    trycatch addresszone

--- api/models/AddressZone.js
+++ api/models/AddressZone.js
@@ -7,43 +7,61 @@ class AddressZone {
     }
 
     static async getAll() {
-        const response = await db.query("SELECT * FROM address_zones");
-        return response.rows.map(a => new AddressZone(a));
+        try {
+            const response = await db.query("SELECT * FROM address_zones");
+            return response.rows.map(a => new AddressZone(a));
+        } catch (error) {
+            console.log(error);
+        }        
     }
 
     static async getOneByNumber(number) {
-        const response = await db.query("SELECT * FROM address_zones WHERE zone_number = $1", [zone_number]);
-        if (response.rows.length != 1) {
-            throw new Error("Unable to locate zone.");
-        }
-        return new AddressZone(response.rows[0]);
+        try {
+            const response = await db.query("SELECT * FROM address_zones WHERE zone_number = $1", [number]);        
+            return new AddressZone(response.rows[0]);
+        } catch (error) {
+            console.log(error);
+        }       
     }
 
     static async getOneById(id) {
-        const response = await db.query("SELECT * FROM address_zones WHERE zone_id = $1", [id]);
-        if (response.rows.length != 1) {
-            throw new Error("Unable to locate zone.");
-        }
-        return new AddressZone(response.rows[0]);
+        try {
+            const response = await db.query("SELECT * FROM address_zones WHERE zone_id = $1", [id]);        
+            return new AddressZone(response.rows[0]);
+        } catch (error) {
+            console.log(error);
+        }        
     }
 
     static async create(data) {
-        const {zone_number } = data;
-        let response = await db.query("INSERT INTO address_zones (zone_number) VALUES ($1) RETURNING *;", [zone_number]);
-        return new AddressZone(response.rows[0]);
+        try {
+            const {zone_number } = data;
+            const response = await db.query("INSERT INTO address_zones (zone_number) VALUES ($1) RETURNING *;", [zone_number]);
+            return new AddressZone(response.rows[0]);
+        } catch (error) {
+            console.log(error);
+        }        
     }
 
     async destroy() {
-        await db.query("UPDATE addresses_florin SET zone_id = $1 WHERE zone_id = $2 RETURNING *;", [1, this.id]);
-        await db.query("UPDATE collect_days SET zone_id = $1 WHERE zone_id = $2 RETURNING *;", [1, this.id]);
-        const response = await db.query("DELETE FROM address_zones WHERE zone_id = $1 RETURNING *;", [this.id]);
-        return new AddressZone(response.rows[0]);
+        try {
+            await db.query("UPDATE addresses_florin SET zone_id = $1 WHERE zone_id = $2 RETURNING *;", [1, this.id]);
+            await db.query("UPDATE collect_days SET zone_id = $1 WHERE zone_id = $2 RETURNING *;", [1, this.id]);
+            const response = await db.query("DELETE FROM address_zones WHERE zone_id = $1 RETURNING *;", [this.id]);
+            return new AddressZone(response.rows[0]);
+        } catch (error) {
+            console.log(error);
+        }        
     }
 
-    async update(data) {        
-        const {zone_number} = data;        
-        const response = await db.query("UPDATE address_zones SET zone_number = $1 WHERE zone_id = $2 RETURNING *;", [zone_number, this.id]);
-        return new AddressZone(response.rows[0]);
+    async update(data) { 
+        try {
+            const {zone_number} = data;        
+            const response = await db.query("UPDATE address_zones SET zone_number = $1 WHERE zone_id = $2 RETURNING *;", [zone_number, this.id]);
+            return new AddressZone(response.rows[0]);
+        } catch (error) {
+            console.log(error);
+        }         
     }
 }
 module.exports = AddressZone;

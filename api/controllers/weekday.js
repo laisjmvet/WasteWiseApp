8930d47f826a845502cdc109bcf30ac73b7fd9c0
laisjmvet <laisjmvet@gmail.com>
2023-07-07 19:25:28 +0100

    ?

--- api/controllers/weekday.js
+++ api/controllers/weekday.js
@@ -65,3 +65,9 @@ async function update (req, res) {
 };
 
 module.exports = {showAll, showOneByName, showOneById, create, destroy, update}; 
+
+//these are the technologies we used for the backend and the frontend. 
+
+//i have this picture here to represent the whole Database. the db seems easy to code but the most chalengging part is to actually plan it. So i have a small piece of code here showing from where the front end is getting the information to display the days and the bins that are going to be collected. So starting at line 109 yo can see that each collect day has the bin type, weekday and the zone.
+
+//Another significant part of code is the login part because many thing happens here. First the data from the body to see what is the username that has been inserted and then it looks for this username on the databse. After that it compares if the password in the databse matches the passowrd inserted.  If they dont match, it will be thrown an error and if it matches, a token for this user will be generated adding one more lay of security to the app.
